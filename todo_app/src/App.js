61b06d45fcbfd5fa418bf876458eabd59f1fac57
Ambuj Bhog <ambujbhog@protonmail.com>
2021-02-12 02:21:28 -0500

    Added Material UI and updated UI

--- todo_app/src/App.js
+++ todo_app/src/App.js
@@ -1,13 +1,34 @@
 import './App.css';
+import { Button, FormControl, Input, InputLabel } from '@material-ui/core'; 
 import React, { useState } from 'react';
 
 function App() {
-  const [todos, setTodo] = useState(['Something', 'Whats up','ben']);
+  const [todos, setTodos] = useState(['Something', 'Whats up','ben']);
+  const [input, setInput] = useState('');
+  const addTodo = (event) => {
+    //this will start when we click the button
+    event.preventDefault(); // will stop refresh
+    setTodos([...todos, input]);
+    setInput(''); // clear the input field after clicking submit 
+  }
+  
   return ( 
     <div className="App">
       <h1> Hey there !</h1>
-      <input/>
-      <button>Add todo</button>
+      <form>
+        
+
+        <FormControl>
+          <InputLabel>Write a to-do</InputLabel>
+          <Input value={input} onChange={event => setInput(event.target.value)}/>
+        </FormControl>
+
+
+        <Button disabled={!input} type='submit' onClick={addTodo} variant="contained" color="primary">
+          Add To-do
+          </Button>
+      </form>
+ 
       <ul>
         {todos.map(todo => (
           <li>{todo}</li>
